f7d3b6ae172cf3313c0dfcb72338669c9129b550
Mark <mark.moskalenko@gmai.com>
2021-04-21 13:31:03 +0300

    Added routing and libraries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tickets',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tickets',
+    loadChildren: () => import('./tickets/tickets.module').then((m) => m.TicketsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { TicketsModule } from './tickets/tickets.module';
+import { MatNativeDateModule } from '@angular/material/core';
+import { CoreModule } from './core/core.module';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { ticketsFeatureKey } from './core/tickets';
+import { ticketsReducer } from './core/tickets/tickets.reducer';
 
 @NgModule({
   declarations: [
@@ -10,9 +20,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TicketsModule,
+    MatNativeDateModule,
+    CoreModule,
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
